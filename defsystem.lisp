84a57f300bf842b9fc40d6e8da3999a47039ffc1
Tim Menzies <tim.menzies@gmail.com>
2020-08-22 16:07:27 -0400

    saving

---
+++ defsystem.lisp
@@ -0,0 +1,34 @@
+(defsystem lump
+  :author "Tim Menzies <timm@ieee.org>"
+  :maintainer "Tim Menzies <timm@ieee.org>"
+  :license "MIT"
+  :version "0.2"
+  :homepage "https://github.com/timm/lump"
+  :bug-tracker "https://github.com/timm/lump/issues"
+  :source-control (:git "git@github.com:timm/lump.git")
+  :description "Some of my LISP tricks"
+  :depends-on ()
+  :components ((:module "src"
+                :serial t
+                :components (
+                            (:file "col")
+                            (:file "got")
+                            (:file "is")
+                            (:file "macros")
+                            (:file "my")
+                            (:file "oo")
+                            (:file "os")
+                            (:file "readmes")
+                            (:file "rows")
+                            (:file "sandbox")
+                            (:file "strings")
+                            (:file "yes")
+                            (:file "yes_col")
+                            (:file "yes_data")
+                            (:file "yes_is")
+                            (:file "yes_macros")
+                            (:file "yes_my")
+                            (:file "yes_rows")
+                            (:file "yes_test")
+       )))
+  :long-description #.(uiop:read-file-string (uiop:subpathname *load-pathname* "README.md")))
